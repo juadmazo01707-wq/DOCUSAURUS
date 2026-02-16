35b5fd029aa961523cc76b53be12c83abe1f9184
juadmazo01707-wq <juadmazo01707@gmail.com>
2026-02-16 03:31:01 -0500

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/coders/POISON_AI/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/coders/POISON_AI/node_modules/infima/dist/css/default/default.css"),
-  require("/home/coders/POISON_AI/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/coders/POISON_AI/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/coders/POISON_AI/src/css/custom.css"),
-];
